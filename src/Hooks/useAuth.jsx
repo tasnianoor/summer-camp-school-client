dd0121740c3c93a07468cfefe27d789d3a3f668e
tasnianoor <tasnianoor2405@gmail.com>
2023-06-12 18:47:03 +0600

    admin dashboard added

--- src/Hooks/useAuth.jsx
+++ src/Hooks/useAuth.jsx
@@ -1,11 +1,3 @@
-// import { useContext } from "react"
-// import { AuthContext } from '../Providers/AuthProvider'
-
-// export const useAuth = () => {
-//     const { user, setUser, loading, setLoading, createUser, signInUser, googleSignInUser, signOutUser, updateUserProfile } = useContext(AuthContext)
-//     return { user, setUser, loading, setLoading, createUser, signInUser, googleSignInUser, signOutUser, updateUserProfile }
-// }
-
 import { useContext } from "react"
 import { AuthContext } from '../Providers/AuthProvider'
 
